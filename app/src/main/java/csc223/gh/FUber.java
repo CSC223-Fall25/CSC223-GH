6cf75d3aae7d23c10ee06acdf997df13cf3dd73d
cptareb <greysonhenry1904@gmail.com>
2025-01-29 15:56:46 -0500

    Euclidean completed

---
+++ app/src/main/java/csc223/gh/FUber.java
@@ -0,0 +1,21 @@
+package csc223.gh;
+
+public class FUber {
+    public static float euclidean(float x1, float y1, float x2, float y2) {
+        // Implement the method here
+
+
+        double xs = Math.pow(x2-x1, 2);
+        double ys = Math.pow(y2-y1, 2);
+        return (float) Math.sqrt(xs + ys);
+    }
+
+    // public static float manhattan(float x1, float y1, float x2, float y2) {
+    //     // Implement the method here
+    // }
+
+    public static void main(String[] args){
+        System.out.println(FUber.euclidean(0, 0, 6, 6));
+    }
+}
+
