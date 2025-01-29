5c58c96c7839e2609f9fb30eda17cf557e0bb266
cptareb <greysonhenry1904@gmail.com>
2025-01-29 15:59:28 -0500

    Manhattan done

--- app/src/main/java/csc223/gh/FUber.java
+++ app/src/main/java/csc223/gh/FUber.java
@@ -10,12 +10,14 @@ public class FUber {
         return (float) Math.sqrt(xs + ys);
     }
 
-    // public static float manhattan(float x1, float y1, float x2, float y2) {
-    //     // Implement the method here
-    // }
+    public static float manhattan(float x1, float y1, float x2, float y2) {
+        // Implement the method here
+        return (float) (Math.abs(x2-x1) + Math.abs(y2-y1));
+    }
 
     public static void main(String[] args){
         System.out.println(FUber.euclidean(0, 0, 6, 6));
+        System.out.println(FUber.manhattan(0, 0, 6, 6));
     }
 }
 
